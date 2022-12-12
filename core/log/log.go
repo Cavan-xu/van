6304bef9db3c8d451cd42ce061e4d7db03fe4ef9
cavan.xu <3395058394@qq.com>
2022-12-12 22:33:01 +0800

    feature: log

--- core/log/log.go
+++ core/log/log.go
@@ -1,24 +1,132 @@
 package log
 
-import "log"
+import (
+	"fmt"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+)
+
+type LogLevel int
+
+const (
+	LogLevelDebug LogLevel = 0
+	LogLevelInfo  LogLevel = 1
+	LogLevelWarn  LogLevel = 2
+	LogLevelError LogLevel = 3
+	LogLevelPanic LogLevel = 4
+)
+
+var (
+	LogLevelNameMap = map[LogLevel]string{
+		LogLevelDebug: "debug",
+		LogLevelInfo:  "info",
+		LogLevelWarn:  "warn",
+		LogLevelError: "error",
+		LogLevelPanic: "panic",
+	}
+)
 
 type ILog interface {
-	LogInfo(info string, v ...interface{})
-	LogErr(info string, v ...interface{})
-	LogPanic(info string, v ...interface{})
+	LogDebug(format string, v ...interface{})
+	LogInfo(format string, v ...interface{})
+	LogWarn(format string, v ...interface{})
+	LogErr(format string, v ...interface{})
+	LogPanic(format string, v ...interface{})
 }
 
 type Log struct {
+	FilePrefix string // 日志名称前缀
+	FileSize   int    // 日志文件最大占用空间
+	FileCount  int    // 单日最多日志文件数
+	LogMask    int    // 日志级别
+	sync.Mutex
 }
 
-func (l *Log) LogInfo(info string, v ...interface{}) {
-	log.Printf(info, v)
+func (l *Log) LogDebug(format string, v ...interface{}) {
+	l.log(LogLevelDebug, format, v)
 }
 
-func (l *Log) LogErr(info string, v ...interface{}) {
-	log.Printf(info, v)
+func (l *Log) LogInfo(format string, v ...interface{}) {
+	l.log(LogLevelInfo, format, v)
 }
 
-func (l *Log) LogPanic(info string, v ...interface{}) {
-	log.Println(info, v)
+func (l *Log) LogWarn(format string, v ...interface{}) {
+	l.log(LogLevelWarn, format, v)
+}
+
+func (l *Log) LogErr(format string, v ...interface{}) {
+	l.log(LogLevelError, format, v)
+}
+
+func (l *Log) LogPanic(format string, v ...interface{}) {
+	l.log(LogLevelPanic, format, v)
+}
+
+func (l *Log) ShouldDoLog(logLevel LogLevel) bool {
+	return 0 != (l.LogMask & (1 << logLevel))
+}
+
+func (l *Log) log(logLevel LogLevel, format string, v ...interface{}) {
+	if !l.ShouldDoLog(logLevel) {
+		return
+	}
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	l.Lock()
+	defer l.Unlock()
+
+	curDate := time.Now().Format("2006-01-02")
+	fileName := fmt.Sprintf("%s_%s.log", l.FilePrefix, curDate)
+	fd, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return
+	}
+
+	logLevelName := LogLevelNameMap[logLevel]
+	curTime := time.Now().Format("2006-01-02 15:04:05")
+	_, _ = fd.WriteString(fmt.Sprintf("[%s][%s]", curTime, logLevelName))
+	_, _ = fd.WriteString(fmt.Sprintf(format+"\n", v...))
+	_ = fd.Close()
+	l.shiftFile(fileName)
+}
+
+func (l *Log) shiftFile(filename string) {
+	info, err := os.Stat(filename)
+	if err != nil {
+		return
+	}
+	if info.Size() < int64(l.FileSize) {
+		return
+	}
+
+	extFileName := path.Ext(filename)
+	baseFileName := strings.TrimSuffix(filename, extFileName)
+	for i := l.FileCount - 1; i >= 0; i-- {
+		tmpFileName := fmt.Sprintf("%s%d%s", baseFileName, i, extFileName)
+		if _, err := os.Stat(tmpFileName); err != nil {
+			continue
+		}
+		if i+1 == l.FileCount {
+			_ = os.Remove(tmpFileName)
+		} else {
+			newFileName := fmt.Sprintf("%s%d%s", baseFileName, i+1, extFileName)
+			_ = os.Rename(filename, newFileName)
+		}
+	}
+}
+
+// 获取到代码所在的文件名、行数
+func FileLine() string {
+	if _, fileName, fileLine, ok := runtime.Caller(1); ok {
+		return fmt.Sprintf("%s:%s", path.Base(fileName), fileLine)
+	}
+	return ""
 }
